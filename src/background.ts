0299cf47f5b096b485891f951303e5deb249c53a
Harsh Patel <harsh8221@gmail.com>
2024-10-02 10:57:27 +0530

    Files changed to Typescript

---
+++ src/background.ts
@@ -0,0 +1,23 @@
+chrome.commands.onCommand.addListener(function (command: any) {
+  if (command === "blur_everything") {
+    chrome.tabs.query(
+      { active: true, currentWindow: true },
+      function (tabs: any) {
+        chrome.tabs.sendMessage(tabs[0].id, { action: "blur_everything" });
+      }
+    );
+  } else if (command === "toggle_selection_mode") {
+    chrome.tabs.query(
+      { active: true, currentWindow: true },
+      function (tabs: any) {
+        chrome.tabs.sendMessage(tabs[0].id, {
+          action: "toggle_selection_mode",
+        });
+      }
+    );
+  }
+});
+
+chrome.action.onClicked.addListener(function (tab: any) {
+  chrome.tabs.sendMessage(tab.id, { action: "toggle_selection_mode" });
+});
